2515a9792763d1ab6d4a896066ebb2e1a573cc07
Zaven <kalanderyan99@yandex.ru>
2022-12-11 23:25:28 +0300

    encoding Main.java

--- src/Main.java
+++ src/Main.java
@@ -29,7 +29,7 @@ public class Main {
             } else if (userInput == 0) {
                 break;
             }else{
-                System.out.println("����� ������� ����������, �������� ���� �� ������������");
+                System.out.println("Такая команда отсутсвует. Выберите одну из предложенных.");
             }
         }
     }

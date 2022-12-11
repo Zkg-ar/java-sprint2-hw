7a3fe12b9911f2227aaf799e8c38805a3d858b90
Zkg-ar <63973151+Zkg-ar@users.noreply.github.com>
2022-12-11 23:26:44 +0300

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -29,7 +29,7 @@ public class Main {
             } else if (userInput == 0) {
                 break;
             }else{
-                System.out.println("Такая команда отсутсвует. Выберите одну из предложенных.");
+                System.out.println("Такая команда отсутствует. Выберите одну из предложенных.");
             }
         }
     }
